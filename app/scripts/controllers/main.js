cee0475a9c297c84cf2f1719c9c9331f031f8942
Marco Aurélio Batista Corrêa <marcoabcorrea@gmail.com>
2018-11-17 00:36:01 -0200

    Completing project with mock

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,21 +3,19 @@
 angular.module('bookStoreApp')
   .controller('MainCtrl', function($scope, $http) {
     var generateData = function() {
-      var arr = [];
-      var letterWords = ["alpha", "bravo", "charlie", "daniel", "earl", "fish", "grace", "henry", "ian", "jack", "karen", "mike", "delta", "alex", "larry", "bob", "zelda"]
-      for (var i = 1; i < 60; i++) {
-        var id = letterWords[Math.floor(Math.random() * letterWords.length)];
-        arr.push({
-          "id": id + i,
-          "title": "name " + i,
-          "author": "author " + i,
-          "description": "Description of item #" + i,
-          "published": "published" + i,
-          "rating": "rating " + id,
-          "buy": "Amazon: " + i,
-          "bookCover": "https://media.giphy.com/media/12cfPQIOSlNj8Y/giphy.gif"
-        });
-      }
+      var id = 0;
+      var arr = [{
+        "id": id ++,
+        "title": "In Search of Lost Time",
+        "author": "Marcel Proust",
+        "description": "In The Guermantes Way Proust's narrator recalls his initiation into the dazzling world of Parisian high society. Looking back over his time in the glamorous salons of the aristocracy, he satirises this shallow world and his own youthful infatuation with it. His observations, and his experiences with his lover Albertine, also educate him in the volatile nature of desire as he walks the path towards adulthood.",
+        "published": 1913,
+        "rating": 9.8,
+        "buy": [{"name": "Amazon", "url": "https://ler.amazon.com.br/kp/embed?asin=B003FXCSFO&preview=newtab&linkCode=kpe&ref_=cm_sw_r_kb_dp_tZ37BbACMD45K"},
+                {"name": "Play Store", "url": "https://play.google.com/store/books/details/Marcel_Proust_Swann_s_Way?id=RSLvYXaHbtoC"}],
+        "bookCover": "https://images.gr-assets.com/books/1452956236l/12749.jpg"
+      }];
+
       return arr;
     }
     $scope.allBooks = generateData();
